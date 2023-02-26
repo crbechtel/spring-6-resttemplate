6dd4c1b95e61a7446eec5c0d0120f71e9b9cf1bd
Clifford <crbechtel@gmail.com>
2023-02-26 09:56:53 -0500

    setup client

---
+++ src/main/java/com/cliffordbechtel/spring6resttemplate/client/BeerClient.java
@@ -0,0 +1,9 @@
+package com.cliffordbechtel.spring6resttemplate.client;
+
+import com.cliffordbechtel.spring6resttemplate.model.BeerDTO;
+import org.springframework.data.domain.Page;
+
+public interface BeerClient {
+
+    Page<BeerDTO> listBeers();
+}
